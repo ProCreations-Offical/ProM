df8b4db5abf1d12ceda0d8a50aa7222c52fa1dd3
Glitch (glitch-hello-node) <none>
2024-03-03 20:45:10 +0000

    🏬💗 Checkpoint ./server.js:61706925/4012 ./package.json:61706925/2570

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fastify = require('fastify')();
 const fs = require('fs');
 const path = require('path');
-const sharp = require('sharp'); // Using Sharp library
+const Jimp = require('jimp'); // Using Jimp library
 
 fastify.register(require('@fastify/multipart'));
 
@@ -14,30 +14,32 @@ fastify.post('/upload-image', async (req, reply) => {
     }
 
     // Read uploaded image and iPhone 15 pro frame
-    const uploadedImage = await sharp(data.file);
-    const frameImage = await sharp(path.join(__dirname, 'iphone-15-pro-frame.jpg'));
+    const uploadedImage = await Jimp.read(data.file);
+    const frameImage = await Jimp.read(path.join(__dirname, 'https://i.ibb.co/6ryRyHL/unnamed.jpg'));
 
-    // Get image metadata (width and height)
-    const uploadedMetadata = await uploadedImage.metadata();
-    const frameMetadata = await frameImage.metadata();
+    // Get image dimensions
+    const uploadedWidth = uploadedImage.bitmap.width;
+    const uploadedHeight = uploadedImage.bitmap.height;
+    const frameWidth = frameImage.bitmap.width;
+    const frameHeight = frameImage.bitmap.height;
 
     // Resize uploaded image to fit within the frame while maintaining aspect ratio
     let resizedImage;
-    if (uploadedMetadata.width > uploadedMetadata.height) {
-      resizedImage = await uploadedImage.resize({ width: frameMetadata.width });
+    if (uploadedWidth / uploadedHeight > frameWidth / frameHeight) {
+      resizedImage = await uploadedImage.resize(frameWidth, Jimp.AUTO);
     } else {
-      resizedImage = await uploadedImage.resize({ height: frameMetadata.height });
+      resizedImage = await uploadedImage.resize(Jimp.AUTO, frameHeight);
     }
 
     // Composite resized image onto the frame with appropriate positioning
-    const finalImage = await frameImage
-      .composite([resizedImage], {
-        top: (frameMetadata.height - resizedImage.height) / 2,
-        left: (frameMetadata.width - resizedImage.width) / 2,
-      })
-      .toBuffer();
+    const compositeImage = new Jimp(frameWidth, frameHeight);
+    const xOffset = (frameWidth - resizedImage.bitmap.width) / 2;
+    const yOffset = (frameHeight - resizedImage.bitmap.height) / 2;
+    await compositeImage.composite(resizedImage, xOffset, yOffset);
 
-    fs.writeFileSync('output.jpg', finalImage);
+    // Convert composite image to buffer and save
+    const buffer = await compositeImage.getBufferAsync(Jimp.MIME_JPEG);
+    fs.writeFileSync('output.jpg', buffer);
 
     reply.code(200).send({ message: 'Image processed and frame integrated successfully!' });
   } catch (error) {
